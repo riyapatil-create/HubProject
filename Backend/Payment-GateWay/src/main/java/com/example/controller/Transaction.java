84d1aa5a136b70161d4e425d2dba2a597c3b07fa
Riya Patil (NCS) <riya.patil@in.ncs-i.com>
2023-07-25 11:51:56 +0530

    final commit

---
+++ Backend/Payment-GateWay/src/main/java/com/example/controller/Transaction.java
@@ -0,0 +1,10 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+
+public class Transaction {
+
+	public void createTransaction(@PathVariable(name="")Double amount) {
+		
+	}
+}
---
+++ Backend/Payment-GateWay/src/main/java/com/example/transactionservice/TransactionService.java
@@ -0,0 +1,20 @@
+package com.example.transactionservice;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionService {
+	
+	
+	private static final String KEY= "rzp_test_3PjOYjLwY6rtbs";
+	private static final String KEY_SECRET= "TxW3eDNmi6MYUtbYCtWdpZYU";
+	private static String Current="INR";
+			
+	
+	public void createTransaction(Double amount) {
+		
+	
+		
+	}
+
+}
